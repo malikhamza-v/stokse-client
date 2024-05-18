e32d07f1c5b05c64617bfde9ac9ffeb36a91909e
Malik Hamza <malikhamza.v@gmail.com>
2024-05-19 00:23:59 +0500

    fixed catalogue view

--- src/renderer/views/catalogue/Catalogue.tsx
+++ src/renderer/views/catalogue/Catalogue.tsx
@@ -2,15 +2,44 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-nested-ternary */
-import RecentOrders from '../../components/viewComponents/catalogue/RecentOrders';
-import RecentCustomer from '../../components/viewComponents/catalogue/RecentCustomers';
+import { useState } from 'react';
+import { Customers } from '../customer';
+import Orders from '../orders/Orders';
 
 /* eslint-disable jsx-a11y/control-has-associated-label */
 export default function Catalogue() {
+  const [selectedCatalogue, setSelectedCatalogue] = useState('order');
+
   return (
-    <section className="container p-10 mx-auto overflow-y-scroll flex flex-col gap-20">
-      <RecentOrders />
-      <RecentCustomer />
-    </section>
+    <div className="grid grid-cols-12 w-full">
+      <div className="col-span-2 border-r ">
+        <p className="font-medium pt-8 px-8">Catalogue</p>
+        <div className="px-2 mt-2">
+          <div
+            className={`hover:bg-slate-100 cursor-pointer transition-all duration-300 p-4 rounded-lg px-6 ${
+              selectedCatalogue === 'order'
+                ? 'bg-purple-100 hover:bg-purple-100'
+                : 'bg-transparent'
+            }`}
+            onClick={() => setSelectedCatalogue('order')}
+          >
+            <p className="font-light text-sm">Orders</p>
+          </div>
+          <div
+            className={`hover:bg-slate-100 cursor-pointer transition-all duration-300 p-4 rounded-lg px-6 ${
+              selectedCatalogue === 'customer'
+                ? 'bg-purple-100 hover:bg-purple-100'
+                : 'bg-transparent'
+            }`}
+            onClick={() => setSelectedCatalogue('customer')}
+          >
+            <p className="font-light text-sm">Customers</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-10 ">
+        {selectedCatalogue === 'order' ? <Orders /> : <Customers />}
+      </div>
+    </div>
   );
 }
